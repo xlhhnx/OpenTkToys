0a4561243c74225ee5aeadb4e093e578181e3eb6
JBStokley@Outlook.com <JBStokley@Outlook.com>
2021-10-19 23:35:45 -0400

    Creates project for drawing tutorials

---
+++ BasicDrawingTester/BasicDrawingTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicDrawingTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
